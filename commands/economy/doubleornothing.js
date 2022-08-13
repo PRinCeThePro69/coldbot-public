ca4b53ffe9691434d366b059bcdd24162cd6ec2e
Deveroonie <74148830+Deveroonie@users.noreply.github.com>
2022-08-13 14:20:02 +0100

    ft: doubleornothing

---
+++ commands/economy/doubleornothing.js
@@ -0,0 +1,44 @@
+const { QuickDB } = require("quick.db");
+const db = new QuickDB();
+const { MessageEmbed } = require("discord.js")
+function RNG(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+module.exports = {
+  category: 'Economy',
+  description: 'Bet some money - you either double it or lose it all', // Required for slash commands
+  
+  slash: false, // Create both a slash and legacy command
+  testOnly: true, // Only register a slash command for the testing guilds
+  expectedArgs: '<user> <amount>',
+  minArgs: 1,
+  maxArgs: 1,
+  callback: async({ message, args }) => {
+	  if(!args.length == 1 || !args[0] || isNaN(args[0])) {
+		  message.reply("Your arguments are invalid")
+	  } else {
+		  const amount = args[0]
+		  const winnings = amount * 2
+		  await(db.sub(`${message.author.id}_bal`, amount))
+		  const loses = RNG(1,2)
+		  if(loses == 1) {
+			  //no money L bozo
+			  const suckstobeu = new MessageEmbed()
+			  .setTitle("Aw man")
+			  .setDescription(`You lost <:icons_money:1005234572914606120> ${amount}`)
+			  .setColor("RED")
+
+			  message.reply({embeds:[suckstobeu]})
+		  } else {
+			  await(db.add(`${message.author.id}_bal`, winnings))
+			  const great = new MessageEmbed()
+			  .setTitle("Well done")
+			  .setDescription(`You won <:icons_money:1005234572914606120> ${winnings}`)
+			  .setColor("GREEN")
+
+			  message.reply({embeds:[great]})
+		  }
+		  
+	  }
+  }
+}
